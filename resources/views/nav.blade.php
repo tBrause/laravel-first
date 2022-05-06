3b8149802f34ff6705d6d28de9c4c3c1f66d9352
tBrause <brause.torsten@gmail.com>
2022-05-07 00:22:39 +0200

    class from bootstrap

--- resources/views/nav.blade.php
+++ resources/views/nav.blade.php
@@ -1,8 +1,13 @@
-<nav>
-    <ul>
-        <li><a href="/">Startseite</a></li>
-        <li><a href="/products">Produkte</a></li>
-        <li><a href="/productdetails/1">Produktdetail</a></li>
-        <li><a href="/product/create">Produkt hinzufügen</a></li>
-    </ul>
+<nav class="navbar navbar-default">
+    <div class="container-fluid">
+      <div class="navbar-header">
+        <a class="navbar-brand" href="#">WebSiteName</a>
+      </div>
+      <ul class="nav navbar-nav">
+        <li><a class="my-2 text-dark" href="/">Startseite</a></li>
+        <li><a class="my-2 text-dark" href="/products">Produkte</a></li>
+        <li><a class="my-2 text-dark" href="/productdetails/1">Produktdetail</a></li>
+        <li><a class="my-2 text-dark" href="/product/create">Produkt hinzufügen</a></li>
+      </ul>
+    </div>
 </nav>
